9fb5e489d1f04941f4eff8d704a4ca9af80d861c
Mayra Urquiza <mayurquiza95@gmail.com>
2024-05-06 20:58:34 -0300

    agrego caso de prueba para cuando no se proporcionó api key

--- tests/common.spec.ts
+++ tests/common.spec.ts
@@ -1,7 +1,9 @@
 import should from "should";
+import sinon from "sinon";
 import { Response } from "supertest";
 import { request, shouldBeJsonContent, shouldBeStatus } from ".";
 import { API_PATH } from "../src/config/default";
+import * as secrets from "../src/config/secrets";
 
 describe("Not implemented endpoint", () => {
   let response: Response;
@@ -21,5 +23,35 @@ describe("Not implemented endpoint", () => {
       "message",
       `Cannot GET ${API_PATH}not-implemented`
     );
+  });
+});
+
+describe("No API KEY available", () => {
+  let response: Response;
+
+  before(async () => {
+    sinon.stub(secrets, "OPEN_WEATHER_API_KEY").set(() => "");
+    response = await request.get(`${API_PATH}current`);
+  });
+
+  it("Should return 401 status code", () => shouldBeStatus(response, 401));
+
+  it("Should return json format data", () => shouldBeJsonContent(response));
+
+  it("Should return error data", () => {
+    should(response.body).be.instanceOf(Object);
+    should(response.body).have.property("code", 401);
+    should(response.body).have.property(
+      "message",
+      "There is no API KEY available"
+    );
+    should(response.body).have.property(
+      "details",
+      "Must provide Open Weather API KEY"
+    );
+  });
+
+  after(() => {
+    sinon.restore();
   });
 });
