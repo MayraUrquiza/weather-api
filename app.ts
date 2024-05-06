56c4caabbf765d920c1f37427a518f7d59a19f15
Mayra Urquiza <mayurquiza95@gmail.com>
2024-05-06 20:56:27 -0300

    muevo puerto a variables de configuración

--- app.ts
+++ app.ts
@@ -6,10 +6,9 @@ import {
 } from "./src/middlewares/errorsMiddleware";
 import { router } from "./src/routes/index";
 import { logger } from "./src/utils/logger";
-import { API_PATH } from "./src/config/default";
+import { API_PATH, PORT } from "./src/config/default";
 
 export const app = express();
-const port = 3000;
 
 app.use(json());
 app.use(helmet());
@@ -17,6 +16,6 @@ app.use(errorMiddleware);
 app.use(API_PATH, router);
 app.use(notFoundMiddleware);
 
-app.listen(port, () => {
-  logger.info(`Server listening on port ${port}`);
+app.listen(PORT, () => {
+  logger.info(`Server listening on port ${PORT}`);
 });
--- src/config/default.ts
+++ src/config/default.ts
@@ -1,4 +1,4 @@
 export const IP_API_URL = "http://ip-api.com";
-export const OPEN_WEATHER_API_KEY = "c66451b944a6fc6231dcb8d25a098255";
 export const OPEN_WEATHER_API_URL = "https://api.openweathermap.org";
 export const API_PATH = "/v1/";
+export const PORT = 3000;
